ddf9b8e4f052d00e2fe3debbdaee599ac348c7f1
Titouan Mathis <titouan@studiometa.fr>
2018-11-19 16:36:18 +0100

    Add package.json file and project configuration

--- base.js
+++ base.js
@@ -1,21 +1,20 @@
 module.exports = {
-	root: true,
-	extends: [
-		'eslint:recommended',
-		'airbnb',
-		'prettier',
-	],
-	rules: {
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-	},
-	plugins: ['prettier'],
-	parserOptions: {
-		parser: 'babel-eslint',
-		ecmaVersion: 2017,
-		sourceType: 'module',
-	},
-	env: {
-		es6: true,
-	},
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'eslint-config-airbnb-base',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2017,
+    sourceType: 'module',
+  },
+  env: {
+    es6: true,
+  },
 };
