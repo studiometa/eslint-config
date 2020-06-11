4ac3bc4021985f9703f8bdfcfc4852aa8cf074e5
Titouan Mathis <titouan@studiometa.fr>
2020-06-11 15:15:42 +0200

    Lint the Vue test file

--- test/vue/test.vue
+++ test/vue/test.vue
@@ -3,21 +3,21 @@
 </template>
 
 <script>
-export default {
-  name: 'FooBar',
-  data() {
-    return {
-      dd: 'foo',
-      isActive: false,
-    };
-  },
-  created() {
-    console.log('created');
-  },
-  methods: {
-    toggle() {
-      this.isActive = !this.isActive;
+  export default {
+    name: 'FooBar',
+    data() {
+      return {
+        dd: 'foo',
+        isActive: false,
+      };
     },
-  },
-};
+    created() {
+      console.log('created');
+    },
+    methods: {
+      toggle() {
+        this.isActive = !this.isActive;
+      },
+    },
+  };
 </script>
