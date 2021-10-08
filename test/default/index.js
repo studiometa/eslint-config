60d186859845ec51ddb4f2e2e08e7426b361cee9
Titouan Mathis <titouan@studiometa.fr>
2021-10-08 09:45:31 +0200

    Add import tests

--- test/default/index.js
+++ test/default/index.js
@@ -1,7 +1,16 @@
+import Component from './component.js';
+
 /**
  * Class Foo.
  */
 export default class Foo {
+  /**
+   * @type {Record<string, typeof Component>}
+   */
+  components = {
+    Component,
+  };
+
   /**
    * Class constructor.
    */
