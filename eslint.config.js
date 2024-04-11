8ac5ee5149e35dba45573985ac3ae38d82f5f686
Titouan Mathis <titouan@studiometa.fr>
2024-04-11 17:31:01 +0200

    Add a `global` exports to easier global definitions

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,16 @@
 import { js, vue, prettier } from './src/index.js';
+import { globals } from './src/utils.js';
 
 export default [
   ...js,
   ...vue,
   ...prettier,
+  {
+    files: ['test/build-files/**/*'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
 ];
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+import globals from 'globals';
+
+export { globals };
